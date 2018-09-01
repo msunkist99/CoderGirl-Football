de180baa8bf84e3c43e6e6f4e6bdf1d76195d4f6
Scott Kuhl <scott@kuhl.ws>
2018-09-01 00:44:42 -0500

    Add project files.

---
+++ Football/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Football
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
