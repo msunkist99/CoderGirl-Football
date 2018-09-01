21e8fb7344099b1e8355f86af07ba8ffdec2dddc
Scott Kuhl <scott@kuhl.ws>
2018-09-01 00:51:22 -0500

    Initial project.

--- Football/Program.cs
+++ Football/Program.cs
@@ -2,11 +2,22 @@
 
 namespace Football
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter the number of touchdowns: ");
+            int touchdowns = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the number of field goals: ");
+            int fieldgoals = int.Parse(Console.ReadLine());
+
+            // Compute the number of points in a single line of code.
+            int points = 0;
+
+            Console.WriteLine($"The team scored {points} points.");
+
+            Console.ReadLine();
         }
     }
 }
